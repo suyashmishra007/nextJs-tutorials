4b231946f574b8a569a830cda7a56c233740fd2c
Suyash <suyashmishra370@gmail.com>
2023-04-02 23:53:54 +0530

    completed till 76

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>HomePage</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 };
