134240b882b6fdc25229536c384f0338ce16b0bf
Suyash <suyashmishra370@gmail.com>
2023-04-13 00:12:35 +0530

    completed till 102

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
+import Link from "next/link";
+import path from "path";
+import fs from "fs";
+
 function HomePage(props) {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
 }
 
+export async function getStaticProps(context) {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFileSync(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: "/no-data",
+    };
+  }
+  if (data.products.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
 export default HomePage;
