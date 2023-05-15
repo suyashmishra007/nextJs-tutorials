9a07e11711438e986199fa64093163b26c1e6afa
Suyash <suyashmishra370@gmail.com>
2023-05-16 00:22:52 +0530

    adding API route ,completed till  197

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
-import React from "react";
-
+import React, { Fragment } from "react";
+import ContactForm from "../components/contact/contact-form";
+import Head from "next/head";
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages!" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 };
 
 export default ContactPage;
